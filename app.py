5e44aefad947b6b72b3433f3da6eadfc1a53829f
HETTski <tanski.hubo@gmail.com>
2025-03-26 18:11:42 +0100

    feat: complete version 1

--- app.py
+++ app.py
@@ -13,3 +13,30 @@ plateWidth = 50e-2
 plateHeight = 50e-2
 holeWidth = 2e-2
 holeHeight = 3e-2
+
+spacing = 0
+Nholes = 0
+S = 0
+
+while S < 8:
+    spacing += 1e-3
+    cols = int(plateWidth // (holeWidth + spacing))
+    rows = int(plateHeight // (holeHeight + spacing))
+    Nholes = cols * rows
+    S = maskingEfficiency(Nholes, lambdaP)
+
+print (f'Liczba otworów: {Nholes}, Skuteczność ekranowania: {S:.2f} dB, Optymalny odstęp: {spacing*100:.1f} mm')
+
+fig, ax = plt.subplots(figsize=(6,6))
+for i in range(rows):
+    for j in range(cols):
+        x = j * (holeWidth + spacing)
+        y = i * (holeHeight + spacing)
+        rect = plt.rectangle((x,y), holeWidth, edgecolor="black", facecolor='gray')
+
+ax.set_xlim(0, plateWidth)
+ax.set_ylim(0, plateHeight)
+ax.set_title("Rozmieszczenie otworów w przesłonie")
+ax.set_aspect('equal')
+plt.gca().invert_yaxis()
+plt.show()
